17e10477ba3f3ee9d6b497fa10fd8234132c079b
Ale <alesullings@gmail.com>
2020-10-20 20:18:06 -0300

    added hamburger menu toggle

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,11 +5,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 class Navbar extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuToggleOn: false
+    }
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      menuToggleOn: !state.menuToggleOn
+    }));
+  }
+
   render() {
+    const {menuToggleOn} = this.state;
+    
     return(
       <>
         <header className="header">
-          <FontAwesomeIcon icon={faBars} className="hamburgerMenu" />
+          <FontAwesomeIcon icon={faBars} className="hamburgerMenu" onClick={() => this.handleClick()} />
           <Link to="/">
             <img className="courflixLogo" src="https://i.postimg.cc/0547t0FP/courflix-logo.png" />
           </Link>
@@ -21,15 +38,17 @@ class Navbar extends React.Component {
             <a className="sectionLink">Mi lista</a>
           </nav>
         </header>
-        <header className="mobileHeader">
-          <nav className="mobileNavbar">
-            <a href="#" className="sectionLink">Inicio</a>
-            <a href="#Series" className="sectionLink">Series</a>
-            <a href="#Peliculas" className="sectionLink">Peliculas</a>
-            <a href="#Recientes" className="sectionLink">Seguir viendo</a>
-            <a className="sectionLink">Mi lista</a>
-          </nav>
-        </header>
+        {menuToggleOn && (
+          <header className="mobileHeader">
+            <nav className="mobileNavbar">
+              <a href="#" className="sectionLink">Inicio</a>
+              <a href="#Series" className="sectionLink">Series</a>
+              <a href="#Peliculas" className="sectionLink">Peliculas</a>
+              <a href="#Recientes" className="sectionLink">Seguir viendo</a>
+              <a className="sectionLink">Mi lista</a>
+            </nav>
+          </header>
+        )}
       </>
     )
   }
